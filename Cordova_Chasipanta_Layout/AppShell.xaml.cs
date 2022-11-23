023d1d62711e54e0e84ecdaa9657bb51db6c33af
Amely Cordova <amely.cordova@udla.edu.ec>
2022-11-23 08:36:30 -0500

    Agregar archivos de proyecto.

---
+++ Cordova_Chasipanta_Layout/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Cordova_Chasipanta_Layout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Cordova_Chasipanta_Layout/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Cordova_Chasipanta_Layout;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
